78e7a593042f54afe78c7f7599f4fe5160eb04b8
Aryan Tripathi <aryantripathi515@gmail.com>
2026-09-23 19:06:21 -0700

    Create day2.java

---
+++ day2.java
@@ -0,0 +1,32 @@
+// Naming convention in java 
+/* 
+class: name starts with capital letter.
+method: name starts with lowercase letter.
+variable: name starts with lowercase letter.
+constant: name Capital letter.
+packages: name lowercase latter.
+*/
+//EXAMPLE
+// class test{}
+// void m1(){}
+// int firstName;
+// int AGE = 20;
+// import java.io.*;
+
+//EXAMPLE CODE
+class Test {
+
+    static void m1(){
+    System.out.println("m1() called");
+    }
+
+    public static void main(String ar[]){
+    int a;
+    int AGE = 20;
+    System.out.println(AGE);
+    m1();
+    
+
+    }
+
+}
