f94e8e711ba20d358a44a7b7dda6939d40652ee4
Aryan Tripathi <aryantripathi515@gmail.com>
2026-09-24 08:12:39 -0700

    Create day3.java

---
+++ day3.java
@@ -0,0 +1,15 @@
+//Variable in java 
+//variable is a data container used to store data value in memory.
+
+//Example Code
+
+class Var{
+    static char c = 'A';
+    static int a = 10;
+    static byte b = 1;
+    public static void main(String ar[]){
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
