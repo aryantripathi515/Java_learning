19839ed53331e08405e98dcbfce5c06b9a9262a2
Aryan Tripathi <aryantripathi515@gmail.com>
2026-09-25 00:30:35 -0700

    Create day4.java

---
+++ day4.java
@@ -0,0 +1,27 @@
+//Operators in java
+/*
+
+1.arithmetic
+2.logical
+3.relational
+4.bitwise
+5.unary
+6.tarnery
+7.assignment
+
+*/
+
+
+//Arithmetic Operators
+
+class Oper{
+    public static void main(String ar[]){
+        int a=11;
+        int b=25;
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+    }
+}
